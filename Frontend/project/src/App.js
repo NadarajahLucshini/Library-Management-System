9c587db4e5ee1b2467ff759f675ff01e35fa8b8e
Praveenmoch <mochpraveen36@gmail.com>
2025-06-26 14:19:46 +0530

    add

--- Frontend/project/src/App.js
+++ Frontend/project/src/App.js
@@ -1,13 +1,9 @@
 import LogIn from "./Pages/LogInPage";
-
-
-
+import './Assects/Login.css';
 
 function App() {
   return (
     <LogIn></LogIn>
-    
-    
   );
 }
 
--- Frontend/project/src/Pages/LogInPage.jsx
+++ Frontend/project/src/Pages/LogInPage.jsx
@@ -1,8 +1,37 @@
 function LogIn() {
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log("Form submitted");
+  
+  };
+
   return (
-    <div class = "login">
-        <h1><center>LOGIN</center></h1>
+    <div class="form">
+      <div class="welcometext">
+        <h1 id="A">Welcome to the Library System</h1>
+        <p id="B">Log in to continue...</p>
+      </div>
+      <div class="loginbox">
+        <h2 id="login">Log in</h2>
+        <form onSubmit={handleSubmit}>
+
+          
+          <label id="label" htmlFor="username"><b>Username</b></label> 
+          <input type="text" id="username" name="username" placeholder="Username" required />
         
+          
+          <label id="label" htmlFor="password"><b>Password</b></label>         
+          <input type="password" id="password" name="password" placeholder="Password" required />
+
+          
+          
+          
+          
+
+          <button id="button" type="submit">Log in</button>
+          <p class="signup"><b>Don't have an account? <a href="#">Sign up now</a></b></p>
+        </form>
+      </div>
     </div>
   );
 }
